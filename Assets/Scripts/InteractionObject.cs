549f5435ed89e551241a25b960e04930a17e25f8
Ashley <asbe1eb@student.oaisd.org>
2024-03-26 14:25:11 -0400

    added more stuff

---
+++ Assets/Scripts/InteractionObject.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+public class InteractionObject : MonoBehaviour
+{
+    [SerializeField] private string interactionText = "I'm an interactable object!";
+
+    public UnityEvent onInteract = new UnityEvent();
+
+    public string GetInteractionText()
+    {
+        return interactionText;
+    }
+
+    public void Interact()
+    {
+        onInteract.Invoke();
+    }
+}
